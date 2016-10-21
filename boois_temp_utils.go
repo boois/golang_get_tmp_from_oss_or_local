7d372581385f646d3497dc44f3ee0f571ff141e3
萧鸣 <boois@qq.com>
2016-10-22 05:26:44 +0800

    first add the func

---
+++ boois_temp_utils.go
@@ -0,0 +1,39 @@
+package boois_temp_utils
+
+import (
+	"io/ioutil"
+	"net/http"
+	"fmt"
+)
+
+var TempList = make(map[string] string)
+
+func GetTemp(file_name string,TempCached bool,OSS_Mode bool,OSS_URL string) string{
+	if !TempCached {
+		delete(TempList,file_name)
+	}
+	if file_content,ok := TempList[file_name];!ok{
+		if OSS_Mode { //从oss中获取
+			resp,err := http.Get(OSS_URL+file_name)
+			if err != nil {
+				fmt.Println("无法获取"+OSS_URL+file_name)
+			}else{
+				defer resp.Body.Close()
+				body,_ := ioutil.ReadAll(resp.Body)
+				file_content = string(body)
+			}
+		}else{ //从文件系统中获取
+			f,file_err := ioutil.ReadFile("template/"+file_name)
+			if file_err == nil{
+				file_content = string(f)
+			}
+		}
+		TempList[file_name] = file_content
+		if OSS_Mode{
+			fmt.Println("没有命中缓存,直接从OSS中读取模板文件!")
+		}else{
+			fmt.Println("没有命中缓存,直接从本地文件系统中读取模板文件!")
+		}
+	}
+	return TempList[file_name]
+}
